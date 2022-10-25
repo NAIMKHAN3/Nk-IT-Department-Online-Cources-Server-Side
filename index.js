cb13691dce673e4a8a0210ca31027406758fd9da
NAIMKHAN3 <naimkhan01856@gmail.com>
2022-10-25 12:29:12 +0600

    full api added

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ const app = express();
 const cors = require("cors");
 const Port = process.env.Port || 5000;
 
+
+const courses = require("./courses.json");
+
 app.get('/', (req, res) => {
     res.send('ami active achi')
+})
+
+app.get('/courses', (req, res) => {
+    res.send(courses)
 })
 
 
