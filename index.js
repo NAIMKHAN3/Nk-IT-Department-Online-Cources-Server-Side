186575092c13ba36b045a4365207f9d54a2aa99e
NAIMKHAN3 <naimkhan01856@gmail.com>
2022-10-25 11:36:05 +0600

    server is running

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-const express = require("express")
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const Port = process.env.Port || 5000;
+
+app.get('/', (req, res) => {
+    res.send('ami active achi')
+})
+
+
+app.listen(Port, () => {
+    console.log('server is running', Port)
+})
